6e942feba95c5fa1ac8cf51c22aaa117e5899484
Jethro Estrada <jethroestrada237@gmail.com>
2025-08-21 12:34:56 +0800

    Updates

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
-sh search/start.sh
-sh scraped_urls/start.sh
-sh ollama_models/start.sh
-sh local_search/start.sh
+# sh search/start.sh
+# sh scraped_urls/start.sh
+# sh ollama_models/start.sh
+# sh local_search/start.sh
+
+# Start redis server at 6379
+redis-stack-server --port 6379 --daemonize yes
