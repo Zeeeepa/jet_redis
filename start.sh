b7c669289cc96267c3c6910685df8f66cd67526f
Jethro Estrada <jethroestrada237@gmail.com>
2025-08-27 00:40:50 +0800

    Updates

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 sh search/start.sh
 sh scraped_urls/start.sh
 sh local_search/start.sh
-# sh ollama_models/start.sh
+sh ollama_models/start.sh
 
 # Start redis server at 6379 for playwright cache
 redis-stack-server --port 6379 --daemonize yes
